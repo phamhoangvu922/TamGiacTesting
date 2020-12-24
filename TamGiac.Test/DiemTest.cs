9a5c976ef035d6bc78e067761f9295758bd6e295
Pham Hoang Vu <37805526+phamhoangvu922@users.noreply.github.com>
2020-12-24 14:26:51 +0700

    demo

--- TamGiac.Test/DiemTest.cs
+++ TamGiac.Test/DiemTest.cs
@@ -11,14 +11,14 @@ namespace TamGiac.Test
         [Test]
         public void ConstructerDiemTest()
         {
-            TamGiac.Diem diem = new TamGiac.Diem();
+            Diem diem = new Diem();
             Assert.AreEqual(0, diem.X, "X khong chinh xac");
             Assert.AreEqual(0, diem.Y, "Y khong chinh xac");
         }
         [Test]
         public void ToStringDiemTest()
         {
-            TamGiac.Diem diem = new TamGiac.Diem(0, 2f);
+            Diem diem = new Diem(0, 2f);
             String test = diem.ToString();
             Assert.AreEqual("0,2", test, "toString khong chinh xac");
         }

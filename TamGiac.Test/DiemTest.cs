f4819b5e32174f6733e24d0e5d835b2196550306
Pham Hoang Vu <37805526+phamhoangvu922@users.noreply.github.com>
2020-12-25 22:41:01 +0700

    final commit

--- TamGiac.Test/DiemTest.cs
+++ TamGiac.Test/DiemTest.cs
@@ -8,11 +8,33 @@ namespace TamGiac.Test
     public class DiemTest
     {
         [Test]
-        public void ConstructerDiemTest()
+        public void ConstructerDiemTestMacDinh()
         {
             Diem diem = new Diem();
             Assert.AreEqual(0, diem.X, "X khong chinh xac");
             Assert.AreEqual(0, diem.Y, "Y khong chinh xac");
+        }
+        [Test]
+        public void ConstructerDiemTest()
+        {
+            Diem diem = new Diem(1.4D,2D);
+            Assert.AreEqual(1.4, diem.X, "X khong chinh xac");
+            Assert.AreEqual(2, diem.Y, "Y khong chinh xac");
+        }
+        [Test]
+        public void ConstructerDiemTestChuoi()
+        {
+            Diem diem = new Diem("2,3");
+            Assert.AreEqual(2, diem.X, "X khong chinh xac");
+            Assert.AreEqual(3, diem.Y, "Y khong chinh xac");
+        }
+        [Test]
+        public void KhoangCachTest()
+        {
+            Diem diemA = new Diem(0,2D);
+            Diem diemB = new Diem(0,4D);
+            double x = diemA.KhoangCach(diemB);
+            Assert.AreEqual(2, x, "Khoang cach khong chinh xac");
         }
         [Test]
         public void ToStringDiemTest()
--- TamGiac.Test/TamGiacTest.cs
+++ TamGiac.Test/TamGiacTest.cs
@@ -1,12 +1,14 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace TamGiac.Test
 {
     [TestFixture]
     class TamGiacTest
     {
+        
         [Test]
-        public void ConstructerTamGiacTest()
+        public void ConstructerTamGiacTestMacDinh()
         {
             TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj();
 
@@ -16,6 +18,95 @@ namespace TamGiac.Test
             Assert.AreEqual(0, tamGiac.B.Y, "Gia tri Y cua diem B khong chinh xac");
             Assert.AreEqual(0, tamGiac.C.X, "Gia tri X cua diem C khong chinh xac");
             Assert.AreEqual(0, tamGiac.C.Y, "Gia tri Y cua diem C khong chinh xac");
+
+        }
+        [Test]
+        public void ConstructerTamGiacTest()
+        {
+            Diem A = new Diem(0, 2D);
+            Diem B = new Diem(1D, 2D);
+            Diem C = new Diem(4D, 0);
+            TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj(A,B,C);
+
+            Assert.AreEqual(0, tamGiac.A.X, "Gia tri X cua diem A khong chinh xac");
+            Assert.AreEqual(2, tamGiac.A.Y, "Gia tri Y cua diem A khong chinh xac");
+            Assert.AreEqual(1, tamGiac.B.X, "Gia tri X cua diem B khong chinh xac");
+            Assert.AreEqual(2, tamGiac.B.Y, "Gia tri Y cua diem B khong chinh xac");
+            Assert.AreEqual(4, tamGiac.C.X, "Gia tri X cua diem C khong chinh xac");
+            Assert.AreEqual(0, tamGiac.C.Y, "Gia tri Y cua diem C khong chinh xac");
+
+        }
+
+        [Test]
+        public void ChuViTest()
+        {
+            Diem A = new Diem(0, 0);
+            Diem B = new Diem(1D, 0);
+            Diem C = new Diem(0, 4D);
+            TamGiacObj tamGiac = new TamGiacObj(A, B, C);
+
+            double chuvi = tamGiac.ChuVi();
+            double kq = Math.Sqrt(17) + 5;
+            Assert.AreEqual(kq, chuvi, "Gia tri chu vi cua Tam Giac khong chinh xac");
+
+        }
+
+        [Test]
+        public void LoaiTamGiacTestCan()
+        {
+            Diem A = new Diem(1D, 2D);
+            Diem B = new Diem(0,0);
+            Diem C = new Diem(2D, 0);
+            TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj(A, B, C);
+
+            int type = tamGiac.LoaiTamGiac();
+
+            Assert.AreEqual(1, type, "Xac dinh loai tam giac can khong chinh xac");
+
+
+        }
+        [Test]
+        public void LoaiTamGiacTestKhongPhai()
+        {
+            Diem A = new Diem(1D, 2D);
+            Diem B = new Diem(0, 0);
+            Diem C = new Diem(1D, 2D);
+            TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj(A, B, C);
+
+            int type = tamGiac.LoaiTamGiac();
+
+            Assert.AreEqual(-1, type, "Xac dinh loai tam giac khong chinh xac");
+
+
+        }
+        [Test]
+        public void LoaiTamGiacTestDeu()
+        {
+            double x = 0.25 * Math.Sqrt(3);
+            Diem A = new Diem(0, 0);
+            Diem B = new Diem(0.5D, 0);
+            Diem C = new Diem(0.25D, x);
+            TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj(A, B, C);
+
+            int type = tamGiac.LoaiTamGiac();
+
+            Assert.AreEqual(2, type, "Xac dinh loai tam giac deu khong chinh xac");
+
+
+        }
+
+        [Test]
+        public void LoaiTamGiacTestThuong()
+        {
+            Diem A = new Diem(0, 0);
+            Diem B = new Diem(0.5D, 0);
+            Diem C = new Diem(0.25D, 1D);
+            TamGiac.TamGiacObj tamGiac = new TamGiac.TamGiacObj(A, B, C);
+
+            int type = tamGiac.LoaiTamGiac();
+
+            Assert.AreEqual(0, type, "Xac dinh loai tam giac thuong khong chinh xac");
+
 
         }
     }
